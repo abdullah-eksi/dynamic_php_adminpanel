11c1643fffcd757be44c80bf1ddb4347aa60aa55
Abdullah Ekşi <104820840+abdullah-eksi@users.noreply.github.com>
2024-07-12 15:47:41 +0300

    Add files via upload

---
+++ aepanel/assets/images/index.php
@@ -0,0 +1,32 @@
+<?php include '../inc/connect.php'; ?>
+<?php include '../inc/fonksiyon.php'; ?>
+
+<?php
+if (isset($_SESSION["kuladi"])) {
+
+$kullanıcı=$_SESSION["kuladi"];
+
+
+$kullanıcıbul=$db->prepare("SELECT*FROM uyeler WHERE uye_kuladi=:adi");
+
+$kullanıcıbul->execute(array(
+'adi'=>$kullanıcı
+));
+$kullanıcıarat=$kullanıcıbul->fetch(PDO::FETCH_ASSOC);
+$yetki=$kullanıcıarat["admin_yetki"];
+/*
+5=developer
+4=admin
+3=Yönetici
+2=editör
+1=üye
+
+*/
+
+}
+else {
+  header("Location:../../login.php?durum=girisyap");
+}
+
+
+ ?>
